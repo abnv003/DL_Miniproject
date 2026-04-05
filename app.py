6d5df25bf91a21b692c6e19471bf4b4811ed3fa1
codewithaddy13 <adheesh.ramdasi@gmail.com>
2026-04-05 18:10:08 +0530

    aunh khant mat exam data removed

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ os.path.dirname("../templates")
 #Flak's Database Configuration
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '127.0.0.1')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'smith03')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'addysql@13')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'examproctordb')
 app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', '3306'))
 mysql = MySQL(app)
